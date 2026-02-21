a67fd4843701306b1d870fe4161fbea54efce392
ivespe-cpu <ivespe@gmail.com>
2026-02-21 16:54:46 -0300

    Update Dashboard.jsx

--- Dashboard.jsx
+++ Dashboard.jsx
@@ -89,33 +89,33 @@ export default function Dashboard() {
 
         {/* MENU DE ESCOLAS */}
         {moduloAtivo === "menu" && (
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {Object.entries(CONFIG_ESCOLAS).map(([nome, dados]) => (
-              <button
-  key={nome}
-  onClick={() => {
-    setEscolaSelecionada(nome);
-    setModuloAtivo("apostilas");
-  }}
-  className="relative h-56 rounded-3xl overflow-hidden border-2 border-orange-600 hover:scale-105 transition"
-  style={{
-    backgroundImage: `url(/jpg/${dados.banner})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  }}
->
-  {/* camada escura */}
-  <div className="absolute inset-0 bg-black/60"></div>
+  <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+    {Object.entries(CONFIG_ESCOLAS).map(([nome, dados]) => (
+      <button
+        key={nome}
+        onClick={() => {
+          setEscolaSelecionada(nome);
+          setModuloAtivo("apostilas");
+        }}
+        className="relative h-56 rounded-3xl overflow-hidden border-2 border-orange-600 hover:scale-105 transition"
+        style={{
+          backgroundImage: `url(/jpg/${dados.banner})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        {/* camada escura */}
+        <div className="absolute inset-0 bg-black/60"></div>
 
-  <div className="relative z-10 h-full flex items-center justify-center">
-    <h2 className="text-xl font-black text-orange-400 text-center">
-      {nome}
-    </h2>
+        <div className="relative z-10 h-full flex items-center justify-center">
+          <h2 className="text-xl font-black text-orange-400 text-center">
+            {nome}
+          </h2>
+        </div>
+      </button>
+    ))}
   </div>
-</button>
-            ))}
-          </div>
-        )}
+)}
 
         {/* APOSTILAS */}
         {moduloAtivo === "apostilas" && (
